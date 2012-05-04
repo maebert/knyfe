673678bdfad7ed059e70104a443058157c47f3ca
Manuel Ebert <manuel.ebert@upf.edu>
2012-05-04 17:03:53 +0200

    Updated Readme from master

--- bake.sh
+++ bake.sh
@@ -3,8 +3,8 @@ git checkout master README.md
 #git checkout master CHANGELOG.md
 markdown2 README.md > tmp_readme
 #markdown2 CHANGELOG.md > tmp_log
-cat templates/header.html tmp_readme tmp_log templates/footer.html > index.html
-rm tmp_readme tmp_log
-#git add index.html
-#git commit -m "Updated Readme from master"
-#git push -u origin gh-pages
+cat templates/header.html tmp_readme  templates/footer.html > index.html
+rm tmp_readme 
+git add index.html
+git commit -m "Updated Readme from master"
+git push -u origin gh-pages
